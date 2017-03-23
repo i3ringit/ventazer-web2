7e6f8624f7acf9fc76222ac8209f28ddd9e9015b
Marcos del Cristo <i3ringit@gmail.com>
2017-03-23 14:19:48 -0500

    Initial commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,11 +23,11 @@ class Header extends React.Component {
           <Navigation />
           <Link className={s.brand} to="/">
             <img src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>Marcos del Cristo</span>
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
+            <h1 className={s.bannerTitle}>Welcome</h1>
+            <p className={s.bannerDesc}>to my portfolio</p>
           </div>
         </div>
       </div>
